bbf337eb81db2648e5178f3365a52ec0dc8b1866
Christian Burkert <burkert@informatik.uni-hamburg.de>
2021-11-29 00:00:00 +0100

    Cleanup

--- sitewatcher/utils.py
+++ sitewatcher/utils.py
@@ -1,6 +1,3 @@
-from selenium.webdriver.chrome.webdriver import WebDriver
-
-
 def shrink_and_scroll_down(driver):
     driver.set_window_size(800, 600)
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
